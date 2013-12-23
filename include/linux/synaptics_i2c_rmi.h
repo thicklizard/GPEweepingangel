c61c38aef1e0bae284496c83a7b7732810f13079
Thicklizard <thicklizard@gmail.com>
2013-12-23 16:23:35 -0600

    add bb mod from tbalden

--- include/linux/synaptics_i2c_rmi.h
+++ include/linux/synaptics_i2c_rmi.h
@@ -178,6 +178,8 @@ enum {
 	FUNCTION
 };
 
+extern uint8_t touchscreen_is_on(void);
+
 #ifdef CONFIG_TOUCHSCREEN_SYNAPTICS_SWEEP2WAKE
 extern void sweep2wake_setdev(struct input_dev * input_device);
 #endif
