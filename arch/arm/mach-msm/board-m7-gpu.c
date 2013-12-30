084d04365b7d5f26922613b3eef25bbd871366a1
Thicklizard <thicklizard@gmail.com>
2013-12-30 10:22:20 -0600

    change clocks to add another step

--- arch/arm/mach-msm/board-m7-gpu.c
+++ arch/arm/mach-msm/board-m7-gpu.c
@@ -196,10 +196,15 @@ static struct kgsl_device_iommu_data kgsl_3d0_iommu_data[] = {
 static struct kgsl_device_platform_data kgsl_3d0_pdata = {
 	.pwrlevel = {
 		{
-			.gpu_freq = 533000000,
-			.bus_freq = 4,
+			.gpu_freq = 512000000,
+			.bus_freq = 5,
 			.io_fraction = 0,
 		},
+		{
+                        .gpu_freq = 450000000,
+                        .bus_freq = 4,
+                        .io_fraction = 0,
+                },
 		{
 			.gpu_freq = 400000000,
 			.bus_freq = 3,
@@ -216,7 +221,7 @@ static struct kgsl_device_platform_data kgsl_3d0_pdata = {
 		},
 	},
 	.init_level = 2,
-	.num_levels = 4,
+	.num_levels = 5,
 	.set_grp_async = NULL,
 	.idle_timeout = HZ/10,
 	.nap_allowed = true,
